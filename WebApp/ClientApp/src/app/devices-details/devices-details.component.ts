70a33cd929e1e04454ccd69ccc6f2f00d589cbe0
Maciej <maciekcymbalista@gmail.com>
2018-11-02 00:12:05 +0100

    proper working charts addet to Device Details

--- WebApp/ClientApp/src/app/devices-details/devices-details.component.ts
+++ WebApp/ClientApp/src/app/devices-details/devices-details.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Chart } from 'chart.js';
+import { forEach } from '@angular/router/src/utils/collection';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-devices-details',
@@ -8,38 +12,67 @@ import { Chart } from 'chart.js';
 })
 export class DevicesDetailsComponent {
   public chart = []
-  public phonesTemp = []
+  public devicesTemp: DeviceTemp[];
+ // public devicesType: string[];
+  public deviceTempToView: DeviceTemp[];
 
   constructor(private _http: HttpClient) { }
 
   weeklyAvgTempForAllDev() {
-    return this._http.get("http://demo9791456.mockable.io/GetAllAvgTempDevices")
+    return this._http.get<DeviceTemp[]>("http://demo9791456.mockable.io/GetAllAvgTempDevices")
       .map(result => result);
   }
 
 
   ngOnInit() {
     this.weeklyAvgTempForAllDev()
-      .subscribe(res => {
+      .subscribe(result => {
 
-        let allTemps = res['temps']//.map(result => result)
-        let allWeekNumbers = Array.from({ length:52 },(v,k)=> k+1)
-        
-        //allTemps.forEach((res) => {
-        //  let jsdate = new Date(res * 1000)
-        //  weatherDates.push(jsdate.toLocaleTimeString('en', { year: 'numeric', month: 'numeric', day: 'numeric' }))
-        //})
+        this.devicesTemp = result['list'];
+        //        this.devicesType = result['list'].map(result => result.type);
 
-        console.log(allTemps);
 
       })
-
   }
 
+
+  deviceTempUpdate(deviceType: any) {
+    this.deviceTempToView = this.devicesTemp.filter((item) => item.type == deviceType);
+    //console.log(this.deviceTempToView);
+
+    let allTemps = this.deviceTempToView[0].temps;
+    let allWeekNumbers = Array.from({ length: 52 }, (v, k) => k + 1)
+
+    this.chart = new Chart('canvas', {
+      type: 'line',
+      data: {
+        labels: allWeekNumbers,
+        datasets: [
+          {
+            label: "Avg Temp",
+            data: allTemps,
+            borderColor: '#19EF0B',
+            backgroundColor: '#19EF0B',
+            fill: false
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: true,
+        }
+      }
+    });
+  }
+
+
+
 }
 
-interface PhoneTemp {
+interface DeviceTemp {
   id: number;
-  name: string;
-  [temps: number]: any;
+  type: string;
+  temps: number[];
 }
+
+
