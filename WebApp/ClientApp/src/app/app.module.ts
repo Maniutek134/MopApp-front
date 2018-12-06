c8d90c016f8426b140f9d5e4700c77c3f247a4e1
Maciej <maciekcymbalista@gmail.com>
2018-12-06 23:19:43 +0100

    auth component added

--- WebApp/ClientApp/src/app/app.module.ts
+++ WebApp/ClientApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ChartComponent } from './chart/chart.component';
 import { DevicesDetailsComponent } from './devices-details/devices-details.component';
 import { AdminComponent } from './admin/admin.component';
 import { LoginComponent } from './login/login.component';
+import { AuthComponent } from './auth/auth.component';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { LoginComponent } from './login/login.component';
     ChartComponent,
     DevicesDetailsComponent,
     AdminComponent,
-    LoginComponent
+    LoginComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
--- WebApp/ClientApp/src/app/login/login.component.ts
+++ WebApp/ClientApp/src/app/login/login.component.ts
@@ -13,6 +13,10 @@ export class LoginComponent implements OnInit {
   }
 
   loginUser(event) {
-    console.log(event)
+    event.preventDefault()
+    const target = event.target
+    const username = target.querySelector('#username').value
+    const password = target.querySelector('#password').value
+    console.log(username,password)
   }
 }
